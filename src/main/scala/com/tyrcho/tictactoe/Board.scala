59ef14fb55a37217be07de6e16c6e11bfe0b3fc0
Michel Daviot <michel@daviot.info>
2020-03-08 21:56:07 +0100

    display won board as single cross or circle

--- src/main/scala/com/tyrcho/tictactoe/Board.scala
+++ src/main/scala/com/tyrcho/tictactoe/Board.scala
@@ -9,30 +9,36 @@ import slinky.web.html.{className, div}
 class Board extends StatelessComponent {
 
   case class Props(
-      boardState: BoardState,
-      handleClick: Int => Unit
-  )
+                    boardState: BoardState,
+                    handleClick: Int => Unit
+                  )
 
   def renderSquare(i: Int) =
     Square(props.boardState.cells(i), () => props.handleClick(i))
 
-  def render = {
-    div(
-      div(className := "board-row")(
-        renderSquare(0),
-        renderSquare(1),
-        renderSquare(2)
-      ),
-      div(className := "board-row")(
-        renderSquare(3),
-        renderSquare(4),
-        renderSquare(5)
-      ),
-      div(className := "board-row")(
-        renderSquare(6),
-        renderSquare(7),
-        renderSquare(8)
+  def render =
+    props.boardState.winner.x match {
+      case None => div(
+        div(className := "board-row")(
+          renderSquare(0),
+          renderSquare(1),
+          renderSquare(2)
+        ),
+        div(className := "board-row")(
+          renderSquare(3),
+          renderSquare(4),
+          renderSquare(5)
+        ),
+        div(className := "board-row")(
+          renderSquare(6),
+          renderSquare(7),
+          renderSquare(8)
+        )
       )
-    )
-  }
+      case _ =>
+        div(className := "board-won")(
+          props.boardState.winner.toString
+        )
+
+    }
 }
